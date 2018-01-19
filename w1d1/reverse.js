0baa575979a9bea41febb810968a78033c227953
Song Gao <song.d.gao@gmail.com>
2018-01-19 03:00:39 +0000

    reversed words

--- w1d1/reverse.js
+++ w1d1/reverse.js
@@ -1,10 +1,9 @@
 var args = process.argv.slice(2);
 
 for (var i = 0; i < args.length; i++) {
-  for (var i = 1; i < word.length; i++) {
-    word += word[0];
-    console.log(word);
-    word = word.substr(1);
-    console.log(word);
-  }
+  var word = args[i];
+
+  var word = word.split('').reverse().join('');
+
+  console.log(word);
 }
