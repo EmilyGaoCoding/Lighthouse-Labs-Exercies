0d10229ae86f4405441e738ffe4d9f871899127f
Song Gao <song.d.gao@gmail.com>
2018-01-19 01:37:37 +0000

    list those who follow someone that doesn't follow them back

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
-var input = [
-  { x: 3, y: 4 },
-  { x: 12, y: 5 },
-  { x: 8, y: 15 }
-];
-var result = input.map(index => Math.sqrt((index.x * index.x) + (index.y * index.y)));
+function time() {
+  console.log(typeof arguments);
+  var array = [];
+  for (var i = 0; i < arguments.length; i++) {
+    console.log(arguments[i]);
+    array.push(arguments[i]);
+  }
+ console.log(array);
+}
 
-console.log(result[0] === 5);
-console.log(result[1] === 13);
-console.log(result[2] === 17);
+time(3, 5);
