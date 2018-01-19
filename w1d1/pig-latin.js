e08df955135c61ca89bdada0047d5f537ed2beed
Song Gao <song.d.gao@gmail.com>
2018-01-19 03:04:54 +0000

    will help you understand pig latin

--- w1d1/pig-latin.js
+++ w1d1/pig-latin.js
@@ -2,11 +2,13 @@ var input = process.argv.slice(2);
 
 function  pigLatin(arr) {
   var output = '';
+
   for (var i = 0; i < arr.length; i++) {
-    arr[i] = arr[i].substr(1) + arr[i].substr(0, 1) + 'ay';
+    arr[i] = arr[i].substr(1) + arr[i].substr(0, 1) + 'ay' + ' ';
     output += arr[i];
   }
-  console.log(output);
+
+  console.log(output.substr(0, output.length - 1));
 }
 
 pigLatin(input);
